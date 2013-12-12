e88e57ec1af100a650ad02ed747ed42a42ae7fec
willettk <willettk@gmail.com>
2013-12-12 01:27:04 -0600

    Added alternate encoding for KW SQL output

--- python/convert2ascii.py
+++ python/convert2ascii.py
@@ -1,21 +1,17 @@
-import unicodedata
 import codecs
 
-fname = "markers.txt"
-f = codecs.open(fname, encoding='utf-8', mode='r')
+fname = "markers.csv"
+with codecs.open(fname, encoding='utf-8', mode='r') as f:
 
-
-ii = 0
-for l in f:
-    l = l.strip()
-    e = l.split(",")
-
-    if len(e[4]) == 1:
-         s = e[0] + "," + e[1] + "," +  e[2] + "," + e[3] + "," + str(ord(e[4]))
-
-    print s
+    next(f)
+    for line in f:
+        l = line.strip()
+        e = l.split(",")
     
-
-f.close()
-
+        if len(e[4]) == 1:      # Encoding for John's MySQL query
+            s = '%s,%s,%s,%s,%s' % (e[0],e[1],e[2],e[3],ord(e[4]))
+            print s
+        elif len(e[4]) == 3:    # Encoding for Kyle's MySQL query
+            s = '%s,%s,%s,%s,%s' % (e[0],e[1],e[2],e[3],ord(e[4][1]))
+            print s
     
