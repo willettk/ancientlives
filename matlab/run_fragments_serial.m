2aafc359e22589d393d5f5f559879f19c749e59e
willettk <willettk@gmail.com>
2013-12-12 01:24:01 -0600

    Extensive commenting added

--- matlab/run_fragments_serial.m
+++ matlab/run_fragments_serial.m
@@ -10,27 +10,34 @@
 %     consensus_chars - list of consensus characters (locations are peaks)
 %     is_visible - true if plot is to be displayed, as well as printed to a file
 
+% Other Matlab routines called in this script:
+
+%   run_kde
+%   generate_consensus_characters
+%   generate_consensus_report
+
 
 warning('off','all');
-load fraglist.txt;
+load fraglist.txt;                                  % list of fragments w/o consensus yet
 frags_to_run = fraglist;
 num_frags_to_run = length( frags_to_run );
 
 
 % load the database
-load ../markers_db2.mat
+load ../markers_db2.mat                             % Copy of markers.txt in matlab form
+                                                    % Data: fragment_ids, user_ids, x, y, chars
 
 
 
 for i = 1 : num_frags_to_run
-    k = fragment_ids == frags_to_run( i );
-    frag_user_ids = user_ids( k );
+    k = fragment_ids == frags_to_run( i );          % Match where this fragment occurs in main database
+    frag_user_ids = user_ids( k );                  % Find corresponding user, x, y, char data
     frag_x = x( k );
     frag_y = y( k );
     frag_chars = chars( k );
-    disp( frags_to_run( i ) );
+    disp( frags_to_run( i ) );                      % Print the fragment being run
 %    for h = [ 8 6 5 3 ]
-    for h = [ 8 ]
+    for h = [ 8 ]                                   % Kernel width
         binsize = 2 * h;
         peak_merge_threshold = h;
         [ frag_x frag_y peaks peak_heights peak_labels frag_chars frag_user_ids ] = run_kde( h, binsize, peak_merge_threshold, frag_x, frag_y, frag_chars, frag_user_ids );
