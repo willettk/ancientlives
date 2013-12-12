ed8b605862e5889360ab69258abf12fca34bde05
willettk <willettk@gmail.com>
2013-12-12 01:28:19 -0600

    Better comments

--- python/statistics.py
+++ python/statistics.py
@@ -2,9 +2,6 @@ import unicodedata
 import codecs
 import sys
 
-fname = "converted_by_frag.csv"
-f = codecs.open(fname, encoding='utf-8', mode='r')
-
 # Empty lists
 userList = []
 userCount = []
@@ -13,43 +10,43 @@ fragmentList = []
 users = []
 clicks = []
 
-# Start fragment, click count at 0
+# Assume that the id of the first fragment is 0
 fragmentCurrent = 0
 clickCount = 0
 
-for line in f:
-    l = line.strip()
-    e = l.split(",")
+fname = "converted_by_frag.csv"
+with codecs.open(fname, encoding='utf-8', mode='r') as f:
 
-    frag  = int(e[1])
-    userList.append(e[0])
+    for line in f:
+        l = line.strip()
+        e = l.split(",")
     
-    # Count transcriptions of each fragment
-    if frag != fragmentCurrent and len(userList) > 1:
-        fragmentList.append(fragmentCurrent)
-        fragmentCurrent = frag
-
-        # find the unique users who classified each fragment
-        ulist = list(set(userList))
-        users.append(ulist)
-        userCount.append(len(ulist))
-        userList = []
+        frag  = int(e[1])
+        userList.append(e[0])
         
-        # update the number of clicks
-        clicks.append(clickCount)
-        clickCount = 0
+        # Update user and click numbers when fragment counting ends
+        if frag != fragmentCurrent and len(userList) > 1:
+            fragmentList.append(fragmentCurrent)
+            fragmentCurrent = frag
     
-    clickCount += 1
+            # find the unique users who classified each fragment
+            ulist = list(set(userList))
+            users.append(ulist)
+            userCount.append(len(ulist))
+            userList = []
+            
+            # update the number of clicks
+            clicks.append(clickCount)
+            clickCount = 0
+        
+        clickCount += 1
 
-f.close()
-    
 # Write the results to a text file in CSV format
 with open("fragmentStatistics.csv", "w") as ff:
     ff.write('fragment,clicks,userCount,clicksPerUser\n')
     
     for fragment,click,usercnt in zip(fragmentList,clicks,userCount):
-        s = '%s,%s,%s,%s\n' % (fragment,click,usercnt,click/usercnt)
+        s = '%i,%i,%i,%i\n' % (fragment,click,usercnt,click/usercnt)
         ff.write(s)
     
-    ff.close()
 
